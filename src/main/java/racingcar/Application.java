4476ff7af8219a75064a18f0867605f6b2b8d0bc
JungYeonWoong <dusdnd0209@naver.com>
2025-10-26 23:39:36 +0900

    feat(app): connect all components and start racing game

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,36 @@
 package racingcar;
 
+import racingcar.controller.RacingGame;
+import racingcar.domain.Car;
+import racingcar.service.InputParser;
+import racingcar.service.InputValidator;
+import racingcar.service.RandomNumberGenerator;
+import racingcar.service.WinnerCalculator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        InputParser parser = new InputParser();
+        InputValidator validator = new InputValidator();
+        RandomNumberGenerator rng = new RandomNumberGenerator();
+        WinnerCalculator winnerCalculator = new WinnerCalculator();
+
+        // 자동차 이름 입력
+        String carInput = inputView.readCarNames();
+        List<String> carNames = parser.parseCarNames(carInput);
+        List<Car> cars = carNames.stream().map(Car::new).toList();
+
+        // 시도 횟수 입력
+        String tryCountInput = inputView.readTryCount();
+        int tryCount = validator.validateTryCount(tryCountInput);
+
+        // 게임 실행
+        RacingGame racingGame = new RacingGame(cars, rng, winnerCalculator, outputView);
+        racingGame.start(tryCount);
     }
 }
