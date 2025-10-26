7a581eb154d722e64599dda5a87e0c35caccbe3b
JungYeonWoong <dusdnd0209@naver.com>
2025-10-26 21:49:15 +0900

    feat(view): add input view to read race attempt count

--- src/test/java/racingcar/view/InputView.java
+++ src/test/java/racingcar/view/InputView.java
@@ -12,7 +12,17 @@ public class InputView {
     public String readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
         String carNamesInput = Console.readLine();
-
         return carNamesInput;
+    }
+
+    /**
+     * 경주 진행 횟수를 콘솔로부터 입력받음
+     *
+     * @return 사용자가 입력한 이동 횟수 문자열
+     */
+    public String readRacingCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String tryCount = Console.readLine();
+        return tryCount;
     }
 }
