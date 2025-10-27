b6c93cf417a60ce729f2aff03c3767106da8c409
JungYeonWoong <dusdnd0209@naver.com>
2025-10-27 21:59:18 +0900

    refactor(controller): Split race flow into smaller methods to reduce indentation and clarify responsibilities

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -22,26 +22,59 @@ public class RacingGame {
     }
 
     /**
-     * 입력된 횟수만큼 경주를 진행
-     *
-     * @param tryCount 라운드(시도) 횟수
+     * 전체 경주를 시작합니다.
      */
     public void start(int tryCount) {
-        for (int i = 0; i < tryCount; i++) {
-            playOneRound();
-        }
-        List<Car> winners = winnerCalculator.findWinners(cars);
-        outputView.printWinners(winners);
+        repeatRace(tryCount);
+        printWinners();
     }
 
     /**
-     * 1라운드를 진행합니다.
+     * 주어진 횟수만큼 경주를 반복합니다.
+     */
+    private void repeatRace(int tryCount) {
+        for (int i = 0; i < tryCount; i++) {
+            playOneRound();
+        }
+    }
+
+    /**
+     * 한 라운드를 진행합니다.
      */
     private void playOneRound() {
+        moveCars();
+        printRoundResult();
+    }
+
+    /**
+     * 모든 자동차를 한 번 이동시킵니다.
+     */
+    private void moveCars() {
         for (Car car : cars) {
-            int randomNumber = randomNumberGenerator.generateRandomNumber();
-            car.move(randomNumber);
+            moveSingleCar(car);
         }
+    }
+
+    /**
+     * 개별 자동차를 이동시킵니다.
+     */
+    private void moveSingleCar(Car car) {
+        int randomNumber = randomNumberGenerator.generateRandomNumber();
+        car.move(randomNumber);
+    }
+
+    /**
+     * 현재 라운드 결과를 출력합니다.
+     */
+    private void printRoundResult() {
         outputView.printCarPosition(cars);
+    }
+
+    /**
+     * 최종 우승자를 계산하고 출력합니다.
+     */
+    private void printWinners() {
+        List<Car> winners = winnerCalculator.findWinners(cars);
+        outputView.printWinners(winners);
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,7 +2,6 @@ package racingcar.view;
 
 import racingcar.domain.Car;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class OutputView {
 
